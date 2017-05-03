b7035159e438823df3f40fececb7037c8bafb9ce
ofus <email = andrew@aljweb.com>
2017-05-03 06:44:04 -0500

    customize

--- install.sh
+++ install.sh
@@ -1,50 +1,23 @@
 #!/usr/bin/env bash
 
+ln -s $HOME/.dotfiles/tmux/tmux.conf.symlink .tmux.conf
+ln -s $HOME/.dotfiles/git/gitconfig.symlink .gitconfig
+ln -s $HOME/.dotfiles/eslintrc.symlink .eslintrc
+ln -s $HOME/.dotfiles/jshintrc.symlink .jshintrc
+ln -s $HOME/.dotfiles/jscsrc.symlink_excluded .jscsrc
+ln -s $HOME/.dotfiles/jsbeautifyrc.symlink .jsbeautifyrc
+ln -s $HOME/.dotfiles/git/gitignore_global.symlink .gitignore_global
+
 command_exists() {
     type "$1" > /dev/null 2>&1
 }
 
 echo "Installing dotfiles."
 
-echo "Initializing submodule(s)"
-git submodule update --init --recursive
-
-source install/link.sh
-
-source install/git.sh
-
-# only perform macOS-specific install
-if [ "$(uname)" == "Darwin" ]; then
-    echo -e "\n\nRunning on OSX"
-
-    source install/brew.sh
-
-    source install/osx.sh
-
-    # create a backup of the original nginx.conf
-    if [ -f /usr/local/etc/nginx/nginx.conf ]; then
-        mv /usr/local/etc/nginx/nginx.conf /usr/local/etc/nginx/nginx.conf.original
-    fi
-
-    ln -s ~/.dotfiles/nginx/nginx.conf /usr/local/etc/nginx/nginx.conf
-    # symlink the code.dev from dotfiles
-    ln -s ~/.dotfiles/nginx/code.dev /usr/local/etc/nginx/sites-enabled/code.dev
-fi
-
-echo "creating vim directories"
-mkdir -p ~/.vim-tmp
-
-if ! command_exists zsh; then
-    echo "zsh not found. Please install and then re-run installation scripts"
-    exit 1
-elif ! [[ $SHELL =~ .*zsh.* ]]; then
-    echo "Configuring zsh as default shell"
-    chsh -s $(which zsh)
-fi
-
-if ! command_exists zplug; then
-    echo "installing zplug, a plugin manager for zsh - http://zplug.sh"
-    git clone https://github.com/zplug/zplug ~/.zplug
+if ! command_exists diff-so-fancy; then
+    echo "installing diff-so-fancy"
+    npm install -g diff-so-fancy
 fi
 
 echo "Done. Reload your terminal."
+
