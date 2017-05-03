580fffe82a5ddb9cc32a8368512c3862563224e3
ofus <email = andrew@aljweb.com>
2017-05-03 07:03:57 -0500

    editorconfig

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ ln -s jshintrc.symlink $HOME/.jshintrc
 ln -s jscsrc.symlink_excluded $HOME/.jscsrc
 ln -s jsbeautifyrc.symlink $HOME/.jsbeautifyrc
 ln -s git/gitignore_global.symlink $HOME/.gitignore_global
+ln -s editorconfig.symlink $HOME/.editorconfig
 
 command_exists() {
     type "$1" > /dev/null 2>&1
