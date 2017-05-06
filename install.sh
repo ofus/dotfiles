96015eae58c8dd128f534de34063c9548bd09a49
Andrew Joseph <andrew@aljweb.com>
2017-05-06 15:52:46 -0500

    fix links

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-ln -s tmux/tmux.conf.symlink $HOME/.tmux.conf
-ln -s git/gitconfig.symlink $HOME/.gitconfig
-ln -s eslintrc.symlink $HOME/.eslintrc
-ln -s jshintrc.symlink $HOME/.jshintrc
-ln -s jscsrc.symlink_excluded $HOME/.jscsrc
-ln -s jsbeautifyrc.symlink $HOME/.jsbeautifyrc
-ln -s git/gitignore_global.symlink $HOME/.gitignore_global
-ln -s editorconfig.symlink $HOME/.editorconfig
+ln -s $HOME/.dotfiles/tmux/tmux.conf.symlink $HOME/.tmux.conf
+ln -s $HOME/.dotfiles/git/gitconfig.symlink $HOME/.gitconfig
+ln -s $HOME/.dotfiles/eslintrc.symlink $HOME/.eslintrc
+ln -s $HOME/.dotfiles/jshintrc.symlink $HOME/.jshintrc
+ln -s $HOME/.dotfiles/jscsrc.symlink_excluded $HOME/.jscsrc
+ln -s $HOME/.dotfiles/jsbeautifyrc.symlink $HOME/.jsbeautifyrc
+ln -s $HOME/.dotfiles/git/gitignore_global.symlink $HOME/.gitignore_global
+ln -s $HOME/.dotfiles/editorconfig.symlink $HOME/.editorconfig
 
 command_exists() {
     type "$1" > /dev/null 2>&1
