9f3da03e027e3010fbe805b32456b64fb05260ed
Andrew Joseph <andrew@aljweb.com>
2019-08-03 10:10:02 -0500

    install vim and tmux plugins
    tweak npm installation
    
    lots of tmux tweaks

--- install.sh
+++ install.sh
@@ -1,43 +1,74 @@
 #!/usr/bin/env bash
 
-[[ ! -e "$HOME/.tmux.conf" ]] && ln -s "$HOME/.dotfiles/tmux/tmux.conf.symlink" "$HOME/.tmux.conf"
-[[ ! -e "$HOME/.gitconfig" ]] && ln -s "$HOME/.dotfiles/git/gitconfig.symlink" "$HOME/.gitconfig"
-[[ ! -e "$HOME/.eslintrc" ]] && ln -s "$HOME/.dotfiles/eslintrc.symlink" "$HOME/.eslintrc"
-[[ ! -e "$HOME/.jshintrc" ]] && ln -s "$HOME/.dotfiles/jshintrc.symlink" "$HOME/.jshintrc"
-[[ ! -e "$HOME/.jscsrc" ]] && ln -s "$HOME/.dotfiles/jscsrc.symlink_excluded" "$HOME/.jscsrc"
-[[ ! -e "$HOME/.jsbeautifyrc" ]] && ln -s "$HOME/.dotfiles/jsbeautifyrc.symlink" "$HOME/.jsbeautifyrc"
-[[ ! -e "$HOME/.gitignore_global" ]] && ln -s "$HOME/.dotfiles/git/gitignore_global.symlink" "$HOME/.gitignore_global"
-[[ ! -e "$HOME/.editorconfig" ]] && ln -s "$HOME/.dotfiles/editorconfig.symlink" "$HOME/.editorconfig"
-[[ ! -e "$HOME/.dircolors" ]] && ln -s "$HOME/.dotfiles/dircolors.symlink" "$HOME/.dircolors"
-[[ ! -e "$HOME/.sqliterc" ]] && ln -s "$HOME/.dotfiles/sqliterc.symlink" "$HOME/.sqliterc"
-
 command_exists() {
     type "$1" > /dev/null 2>&1
 }
 
-function install_dotfiles_package()
-{
+install_dotfiles_package() {
     local PACKAGE=$1
 
-    if ! command_exists $PACKAGE; then
+    if ! command_exists "$PACKAGE"; then
         read -p "Install $PACKAGE? " -n 1 -r
         echo
         if [[ $REPLY =~ ^[Yy]$ ]]; then
-            echo "installing $PACKAGE"
-            npm install -g $PACKAGE
+            if ! command_exists npm; then
+                echo "NPM not found, so we can't install $PACKAGE"
+            else 
+                echo "installing $PACKAGE"
+                npm install -g "$PACKAGE"
+            fi
         fi
     fi
 }
 
+# export TMUX_PLUGIN_MANAGER_PATH=~/.tmux/plugins/tpm/
+
+if command_exists apt; then
+    INSTALL_CMD="sudo apt install -y "
+elif command_exists yum; then
+    INSTALL_CMD="sudo yum install -y "
+elif command_exists apt-cyg; then
+    INSTALL_CMD="apt-cyg install "
+elif command_exists pacman; then
+    INSTALL_CMD="sudo pacman -Sl "    
+else
+    INSTALL_CMD="I don't know how to install software packages on this system, can't install "
+fi
+
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [[ ! -d "$HOME/.tmux/plugins/tpm" ]]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    ~/.tmux/plugins/tpm/bin/install_plugins
+else
+    ~/.tmux/plugins/tpm/bin/update_plugins all
+fi
+
+mkdir -p ~/.oldtmux~
+[[ -h "$HOME/.tmux.conf" ]] && rm ~/.tmux.conf
+[[ -e "$HOME/.tmux.conf" ]] && mv ~/.tmux.conf "$HOME/.oldtmux~/.tmux.conf.bak.$(date +%Y%m%d%H%M%S)~"
+[[ -h "$HOME/.tmux/tmux.conf" ]] && rm ~/.tmux/tmux.conf
+[[ -e "$HOME/.tmux/tmux.conf" ]] && mv ~/.tmux/tmux.conf "$HOME/.tmux/.tmux.conf.bak.$(date +%Y%m%d%H%M%S)~"
+ln -s "$DOTFILES_DIR/tmux/tmux.conf.symlink" ~/.tmux.conf
+[[ ! -e "$HOME/.gitconfig" ]] && ln -s "$DOTFILES_DIR/git/gitconfig.symlink" ~/.gitconfig
+[[ ! -e "$HOME/.gitignore_global" ]] && ln -s "$DOTFILES_DIR/git/gitignore_global.symlink" ~/.gitignore_global
+[[ ! -e "$HOME/.eslintrc" ]] && ln -s "$DOTFILES_DIR/eslintrc.symlink" ~/.eslintrc
+[[ ! -e "$HOME/.jshintrc" ]] && ln -s "$DOTFILES_DIR/jshintrc.symlink" ~/.jshintrc
+[[ ! -e "$HOME/.jscsrc" ]] && ln -s "$DOTFILES_DIR/jscsrc.symlink_excluded" ~/.jscsrc
+[[ ! -e "$HOME/.jsbeautifyrc" ]] && ln -s "$DOTFILES_DIR/jsbeautifyrc.symlink" ~/.jsbeautifyrc
+[[ ! -e "$HOME/.editorconfig" ]] && ln -s "$DOTFILES_DIR/editorconfig.symlink" ~/.editorconfig
+[[ ! -e "$HOME/.dircolors" ]] && ln -s "$DOTFILES_DIR/dircolors.symlink" ~/.dircolors
+[[ ! -e "$HOME/.sqliterc" ]] && ln -s "$DOTFILES_DIR/sqliterc.symlink" ~/.sqliterc
+
 echo "Installing dotfiles."
 
-if [[ ! -e "resources/.added" ]]; then
-    read -p "Install truecolor + italics support? (requires sudo) " -n 1 -r
+if [[ ! -e "$DOTFILES_DIR/resources/.added" ]]; then
+    read -p "Install truecolor + italics support?" -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-      for I in $(ls resources); do sudo tic resources/$I; done
-      echo > "resources/.added"
-      echo "Done"
+        for I in "$DOTFILES_DIR/resources/*"; do tic "$I"; done
+        echo > "$DOTFILES_DIR/resources/.added"
+        echo "Done"
     fi
 fi
 
@@ -45,10 +76,8 @@ if ! command_exists npm; then
     read -p "Install NPM? " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        if ! command_exists npm; then
-            echo "installing npm"
-            sudo apt-get install -y npm
-        fi
+        echo "installing npm..."
+        $INSTALL_CMD npm
     fi
 fi
 
@@ -57,5 +86,16 @@ install_dotfiles_package jscs
 install_dotfiles_package jshint
 install_dotfiles_package eslint
 
-echo "Done. Reload your terminal."
+read -p "Customize Vim? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    if ! command_exists vim; then
+        $INSTALL_CMD vim
+    fi
+    [[ -e "$HOME/.vimrc" ]] && mv ~/.vimrc "$HOME/.vimrc.bak.$(date +%Y%m%d%H%M%S)~"
+    ln -s "$DOTFILES_DIR/vimrc.symlink" ~/.vimrc
+    [[ ! -d "$HOME/.vim/bundle/Vundle.vim" ]] && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
+fi
 
+echo "Done. Reload your terminal."
